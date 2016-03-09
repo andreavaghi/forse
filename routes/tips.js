d6941e8dcee5f558580336dba92525054eb4bef6
Mattia Piccinetti <mattia.piccinetti@gmail.com>
2016-03-10 00:57:36 +0100

    random tips are loaded from file with encoding issue #demo #encoding #fsblendorio

--- routes/tips.js
+++ routes/tips.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+var fs = require('fs')
 
 var conString = process.env.DATABASE_URL;
 var superMegaSecretTip = process.env.SUPER_MEGA_SECRET_TIP;
 
+// https://nodejs.org/docs/latest/api/buffer.html#buffer_buffers_and_character_encodings
+//
+// ascii   - for 7-bit ASCII data only
+// utf8    - Multibyte encoded Unicode characters
+// utf16le - 2 or 4 bytes, little-endian encoded Unicode characters
+// ucs2    - Alias of 'utf16le'
+// base64  - Base64 string encoding
+// binary  - A way of encoding the buffer into a one-byte (latin-1) encoded string
+// hex     - Encode each byte as two hexadecimal characters.
 router.get('/tips/random', (req, res) => {
+  fs.readFile('utils/tips-iso-8859-1.txt', 'utf-8', function (err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    var tips = data.toString().split('\n');
+    res.json({ tip: tips[Math.floor(Math.random() * tips.length)] });
+  });
+});
+
+router.get('/tips/dbrandom', (req, res) => {
   pg.connect(conString, (err, client, done) => {
-    if (err) { return console.error('error fetching client from pool', err); }
+    if (err) { 
+      return console.error('error fetching client from pool', err); 
+    }
 
     client.query('select * from tips', (err, result) => {
       done();
